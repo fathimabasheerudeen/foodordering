e97865b90cf7345fb5e97d97f79a1585a3bdd52f
unknown <bfathima2097@gmail.com>
2022-04-27 22:45:22 -0700

    addeddddd

---
+++ erestaurant/figgy/figgyapp/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from figgyapp.models import Category, Product
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name','slug']
+    prepopulated_fields = {'slug':('name',)}
+admin.site.register(Category,CategoryAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name','price','available','created','updated']
+    list_editable = ['price','available']
+    prepopulated_fields = {'slug':('name',)}
+    list_per_page = 20
+admin.site.register(Product,ProductAdmin)
+
+
---
+++ erestaurant/figgy/figgyapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FiggyappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'figgyapp'
